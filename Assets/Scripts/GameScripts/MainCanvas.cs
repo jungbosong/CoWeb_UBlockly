2417c8a0a85003dfbdeb1e5f9a500c3b78de4bc3
Yoon-Jiyeon <she79560@naver.com>
2022-10-28 21:25:44 +0900

    [UPDATE] 코드 주석 추가 (-지연)

--- Assets/Scripts/GameScripts/MainCanvas.cs
+++ Assets/Scripts/GameScripts/MainCanvas.cs
@@ -8,13 +8,11 @@ public class MainCanvas : MonoBehaviour
     // Game Start 버튼을 눌렀을 때 실행되는 함수
     public void OnClickedGameStartBtn()
     {
-        Debug.Log("Game Start...Move to Stage Scene");
         SceneManager.LoadScene("1_StageScene");
     }
     // Quit 버튼을 눌렀을 때 실행되는 함수
     public void OnClickedQuitButton()
     {
-        Debug.Log("Quit Game");
         Application.Quit();
     }
 }
